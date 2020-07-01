a3e69f8c3a1b51d4ec07591b273645e65871d38f
chirag <chiragm2910@gmail.com>
2020-07-01 10:35:20 +0530

    Register Album and photo model on admin site

--- TaskApp/admin.py
+++ TaskApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Photo, Album
 
-# Register your models here.
+
+class ModelAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(Photo, ModelAdmin)
+admin.site.register(Album, ModelAdmin)
