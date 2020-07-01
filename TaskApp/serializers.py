645c0702541d61e1707a42a4fbf3aeaa3b6aec28
chirag <chiragm2910@gmail.com>
2020-07-01 14:43:36 +0530

    Update serializers

--- TaskApp/serializers.py
+++ TaskApp/serializers.py
@@ -2,16 +2,16 @@ from rest_framework import serializers
 from .models import Photo, Album
 
 
-class AlbumSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Album
-        fields = ('id', 'name', 'description', 'created_at', 'updated_at', 'photos')
-
-
 class PhotoSerializer(serializers.ModelSerializer):
-    albums = AlbumSerializer(many=True, read_only=True)
 
     class Meta:
         model = Photo
         fields = ('id', 'user', 'description', 'image', 'created_at', 'updated_at', 'albums')
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    photos = PhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Album
+        fields = ('id', 'name', 'description', 'created_at', 'updated_at', 'photos')
