11b46388ab56991d3f8874957aaf171d5275807b
chirag <chiragm2910@gmail.com>
2020-07-01 14:43:17 +0530

    Update database schema

--- TaskApp/models.py
+++ TaskApp/models.py
@@ -9,29 +9,27 @@ class TimeStampMixin(models.Model):
         abstract = True
 
 
-class Photo(TimeStampMixin):
-    user = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.URLField(max_length=200)
-
-    class Meta:
-        verbose_name = "Photo"
-        verbose_name_plural = "Photos"
-        ordering = ['-id']
-
-    def __str__(self):
-        return self.user
-
-
 class Album(TimeStampMixin):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
-    photos = models.ManyToManyField(Photo, related_name='albums', blank=True)
 
     class Meta:
         verbose_name = "Album"
         verbose_name_plural = "Albums"
-        ordering = ['-id']
 
     def __str__(self):
         return self.name
+
+
+class Photo(TimeStampMixin):
+    user = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.URLField(max_length=200)
+    albums = models.ManyToManyField(Album, related_name='photos', blank=True)
+
+    class Meta:
+        verbose_name = "Photo"
+        verbose_name_plural = "Photos"
+
+    def __str__(self):
+        return self.user
